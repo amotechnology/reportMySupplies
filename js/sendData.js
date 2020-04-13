edc6d49100dbff118919bec7f386c1ffee6c9093
Ryan Loutos <ryanloutos@icloud.com>
2020-04-13 18:26:19 -0500

    form sends response to database

---
+++ js/sendData.js
@@ -0,0 +1,143 @@
+$(document).ready(function () {
+    const nameInput = $('#exampleInputEmail1');
+    const emailInput = $('#email-input');
+    const locationSelect = $('#location');
+    const faceShieldRadio = $("input[name='face-shield-customRadio']");
+    const isolationMaskRadio = $("input[name='isolation-mask-customRadio']");
+    const n95Radio = $("input[name='n95-customRadio']");
+    const paprRadio = $("input[name='papr-customRadio']");
+    const wipeRadio = $("input[name='wipe-customRadio']");
+    const gownRadio = $("input[name='gown-customRadio']");
+    const additionalInfoInput = $('#additional-text');
+    const submitButton = $('#submit-button');
+
+    let name = '';
+    let email = '';
+    let location = '';
+    let faceShields = 'n/a';
+    let isolationMasks = 'n/a';
+    let n95s = 'n/a';
+    let paprs = 'n/a';
+    let wipes = 'n/a';
+    let gowns = 'n/a';
+    let additionalInfo = '';
+
+    faceShieldRadio.click(function() {
+        let type = 'face-shield';
+        let choice = $(this).attr('id');
+
+        if (choice == `${type}-yes`) {
+            faceShields = 'yes';
+        } 
+        else if (choice == `${type}-no`) {
+            faceShields = 'no';
+        } 
+        else {
+            faceShields = 'n/a';
+        }
+    });
+
+    isolationMaskRadio.click(function() {
+        let type = 'isolation-mask';
+        let choice = $(this).attr('id');
+
+        if (choice == `${type}-yes`) {
+            isolationMasks = 'yes';
+        } 
+        else if (choice == `${type}-no`) {
+            isolationMasks = 'no';
+        } 
+        else {
+            isolationMasks = 'n/a';
+        }
+    });
+
+    n95Radio.click(function() {
+        let type = 'n95';
+        let choice = $(this).attr('id');
+
+        if (choice == `${type}-yes`) {
+            n95s = 'yes';
+        } 
+        else if (choice == `${type}-no`) {
+            n95s = 'no';
+        } 
+        else {
+            n95s = 'n/a';
+        }
+    });
+
+    paprRadio.click(function() {
+        let type = 'papr';
+        let choice = $(this).attr('id');
+
+        if (choice == `${type}-yes`) {
+            paprs = 'yes';
+        } 
+        else if (choice == `${type}-no`) {
+            paprs = 'no';
+        } 
+        else {
+            paprs = 'n/a';
+        }
+    });
+
+    wipeRadio.click(function() {
+        let type = 'wipe';
+        let choice = $(this).attr('id');
+
+        if (choice == `${type}-yes`) {
+            wipes = 'yes';
+        } 
+        else if (choice == `${type}-no`) {
+            wipes = 'no';
+        } 
+        else {
+            wipes = 'n/a';
+        }
+    });
+
+    gownRadio.click(function() {
+        let type = 'gown';
+        let choice = $(this).attr('id');
+
+        if (choice == `${type}-yes`) {
+            gowns = 'yes';
+        } 
+        else if (choice == `${type}-no`) {
+            gowns = 'no';
+        } 
+        else {
+            gowns = 'n/a';
+        }
+    });
+
+    submitButton.click(function() {
+        name = nameInput.val();
+        email = emailInput.val();
+        location = locationSelect.val();
+        additionalInfo = additionalInfoInput.val();
+
+        let data = {
+            "name": name,
+            "email": email,
+            "location": location,
+            "faceShields": faceShields,
+            "isolationMasks": isolationMasks,
+            "n95s": n95s,
+            "paprs": paprs,
+            "wipes": wipes,
+            "gowns": gowns,
+            "additionalResources": additionalInfo
+        }
+
+        let lambdaApiUrl = 'https://5alsy89r1j.execute-api.us-east-2.amazonaws.com/prod/reportmysupplies';
+        fetch(lambdaApiUrl, {
+            mode: 'no-cors',
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        });
+    });
+
+});
--- postSupplies.py
+++ postSupplies.py
@@ -1,7 +1,9 @@
 import pymysql
 import sys
 import logging
+from datetime import datetime
 
+# info to access database....could move this to its own file 
 host = 'reportmysupplies.czvyghkkrqot.us-east-2.rds.amazonaws.com'
 name = 'admin'
 password = 'FezTropic365'
@@ -10,6 +12,7 @@ database = 'ReportMySupplies'
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# connect to database
 try:
     conn = pymysql.connect(host=host, user=name, passwd=password, db=database)
 except pymysql.MySQLError as e:
@@ -19,34 +22,48 @@ except pymysql.MySQLError as e:
 
 logger.info('SUCCESS: Connection to RDS MySQL instance succeeded')
 
+# lambda function to send form response to database
 def postSupplies(event, context):
-    # get the data from the form to stroe in database
-    # name = event['name']
-    # email = event['email']
-    # location = event['location']
-    # faceShields = event['faceShields']
-    # isolationMasks = event['isolationMasts']
-    # n95s = event['n95s']
-    # paprs = event['paprs']
-    # wipes = event['wipes']
-    # gowns = event['gowns']
-    # additionResources = event['additionalResources']
+    
+    # get data from api post
+    name = event['name']
+    email = event['email']
+    location = event['location']
+    faceShields = event['faceShields']
+    isolationMasks = event['isolationMasks']
+    n95s = event['n95s']
+    paprs = event['paprs']
+    wipes = event['wipes']
+    gowns = event['gowns']
+    additionalResources = event['additionalResources']
 
-    name = 'ryan'
-    email = 'ryanloutos@icloud.com'
-
-    addReport = ('INSERT INTO users (name, email) VALUES (%s, %s)')
+    formResponseTemplate = ('''
+        INSERT INTO FormResponses (
+            name, employee_email, location,
+            face_shields, isolation_masks, n95s, paprs,
+            wipes, gowns, additional_resources)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ''')
+    
+    formResponseAnswers = (
+        name, email, location, faceShields,
+        isolationMasks, n95s, paprs, wipes,
+        gowns, additionalResources
+    )
+    
     cur = conn.cursor()
-    cur.execute(addReport, (name, email))
-    # result = cur.fetchone()
-    result = "hey"
+    cur.execute(formResponseTemplate, formResponseAnswers)
+    result = str(cur.fetchone())
     conn.commit()
-
-    response = {}
-    response['headers'] = {}
-    response['headers']['Content-Type'] = 'application/json'
-    response['body'] = {}
-    response['body']['status'] = str(result)
+    cur.close()
+    response = {
+        'headers': {
+            'Content-Type': 'application/json'
+        }, 
+        'body': {
+            'status': result
+        }
+    }
 
     return response
 
