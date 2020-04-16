4320cc6abcdae7ff8972ba4d13165062afe173f2
Olly Cohen <olly.k.cohen@gmail.com>
2020-04-16 10:58:06 -0500

    created config.js for cognito

---
+++ config.js
@@ -0,0 +1,12 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_QdROjUN2n', 
+        userPoolClientId: '1agfdk1v4em1nafn9m0g6hd8s7', 
+        region: 'us-east-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
+
+
