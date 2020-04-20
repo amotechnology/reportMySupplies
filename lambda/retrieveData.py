73d86be4510effbf86a326494a5d92948e79be7c
Daniel Sosebee <sosebee@wustl.edu>
2020-04-19 17:16:21 -0700

    query_type:'all' select *

--- lambda/retrieveData.py
+++ lambda/retrieveData.py
@@ -230,10 +230,16 @@ def submission_full(event):
     return execute(sql)
 
 
+# Get everything!
+def all_submissions(event):
+    return execute("SELECT * FROM FormResponses;")
+
+
 # functions to query the database, or to return different error codes
 query_functions = {
     'count': submission_count,
     'select': submission_full,
+    'all': all_submissions,
     'no_query_type_specified': no_query_type_specified
 }
 
@@ -244,15 +250,3 @@ def retrieve_data(event, context):
     query_function = query_functions[query_type]
     response = query_function(event)
     return json.dumps(response, default = str)
-
-result = retrieve_data({
-    'additional_resources': "no",
-'date_range': "2020-04-18 2020-04-19",
-'division': ["Anesthesiology", "Medicine", "Neurology", "Neuroscience", "Neurosurgery", "Obstetrics and Gynecology", "Ophthalmology and Visual Sciences", "Orthopaedic Surgery", "Otolaryngology", "Pathology and Immunology", "Pediatrics", "Radiation Oncology", "Radiology", "Surgury", "Division of Cardiothoracic Surgery", "Division of General Surgery", "Section of Acute and Critical Care Surgery", "Section of Colon and Rectal Surgery", "Section of Hepatobiliary-Pancreatic & GI Surgery", "Section of Minimally Invasive Surgery", "Section of Surgical Oncology", "Section of Transplant Surgery", "Section of Vascular Surgery", "Division of Pediatric Surgery", "Division of Plastic and Reconstructive Surgery", "Division of Public Health Sciences", "Division of Urologic Surgery", "Allergy and Immunology", "Bioorganic Chemistry and Molecular Pharmacology", "Bone and Mineral Diseases", "Cardiology/Cardiovascular Diseases", "Dermatology", "Endocrinology/Metabolism/Lipid Research", "Gastroenterology", "General Medical Sciences", "General Medicine", "Geriatrics and Nutritional Sciences", "Hematology", "Hospital Medicine", "Infectious Diseases", "Nephrology", "Oncology", "Palliative Medicine", "Pulmonary and Critical Care Medicine", "Rheumatology"],
-'location': ["9200 Cardiology"],
-'ppe': [],
-'query_type': "select",
-'y_axis': "location",
-},0)
-
-print(result)
